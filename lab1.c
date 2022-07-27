38f5245aeee08f20b7033eaa98a6c387e6aa0601
aishez <aishezsingh@gmail.com>
2022-07-27 19:44:35 +0530

    added bubble sort

--- lab1.c
+++ lab1.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
 
+void bubblesort(int size , int array[])
+{
+
+    for (int step = 0; step < size - 1; ++step)
+    {
+
+        for (int i = 0; i < size - step - 1; ++i)
+        {
+
+            if (array[i] > array[i + 1])
+            {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
 void swap(int *a, int *b)
 {
     int temp = *a;
